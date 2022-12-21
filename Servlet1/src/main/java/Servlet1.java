6cac537b41a3abccf56e55f7a8586fc775f6e97e
Matteo Piccinini <marcopiccinini72@gmail.com>
2022-12-21 17:52:28 +0100

    Update

--- Servlet1/src/main/java/Servlet1.java
+++ Servlet1/src/main/java/Servlet1.java
@@ -6,17 +6,18 @@ import java.io.IOException;
 
 @WebServlet(name = "Servlet1", value = "/Servlet1")
 public class Servlet1 extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String nome = "Matteo";
         String path = "nome.jsp";
         request.setAttribute("VariabileNome", nome);
-        RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-        dispatcher.forward(request, response);
+        RequestDispatcher pagina = request.getRequestDispatcher(path);
+        pagina.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+
     }
 }
