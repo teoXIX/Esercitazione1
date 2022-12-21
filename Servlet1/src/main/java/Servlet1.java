731259d986001174b90852d04115f1fc6ade51e4
Matteo Piccinini <marcopiccinini72@gmail.com>
2022-12-21 17:40:59 +0100

    nome JSP e Servlet

---
+++ Servlet1/src/main/java/Servlet1.java
@@ -0,0 +1,21 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "Servlet1", value = "/Servlet1")
+public class Servlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nome = "Matteo";
+        request.setAttribute("VariabileNome", nome);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/nome.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
